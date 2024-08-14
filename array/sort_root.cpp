3348f923f8bf46f02523c0fb70e3cc1728a83a7c
devSayan619 <mainmandal23@gmail.com>
2024-08-14 11:12:52 +0530

    a2z striver

---
+++ array/sort_root.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+class Solution{
+	public:
+		bool check(vector<int>&nums){
+		
+		}
+}
+
+int main(){
+	//check if the array is sorted and rootated not 
+	vector<int>nums;
+	bool result = check(nums);
+	cout<<result<<endl;
+	return 0;
+}
