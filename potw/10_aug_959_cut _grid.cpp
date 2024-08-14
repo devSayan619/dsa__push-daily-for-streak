a66425be84bcf37720a4a8b896241c15ad109ac6
devSayan619 <mainmandal23@gmail.com>
2024-08-14 11:15:05 +0530

    potw

---
+++ potw/10_aug_959_cut _grid.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int regionsBySlashes(vector<string>& grid) {
+  
+}
+
+int main(){
+  vector<string> carr = {"/\\","\\/"};
+  int num = regionsBySlashes(carr);
+  return num;
+}
