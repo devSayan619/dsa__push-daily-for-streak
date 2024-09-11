cd9e645101415e260f2c01379d3245881736c7c9
devSayan619 <mainmandal23@gmail.com>
2024-09-11 19:36:01 +0530

    delete all unessaryfiles

--- topic wise question/graph/graph_vertices.cpp
+++ topic wise question/graph/graph_vertices.cpp
@@ -15,7 +15,10 @@ int main()
   //    adj[v][u]=1;
   //  }
 
-  // using list we can store the graph
+  // using list we can store the graph it's take less time complexity
+  // it's for the undirected graph
+  // it's time compelexity is O(2E)  here E is the edge of the graph
+  // here w
   vector<int> adj[n + 1];
   for (int i = 0; i < m; i++)
   {
@@ -25,5 +28,17 @@ int main()
     adj[u].push_back(v);
     adj[v].push_back(u);
   }
+
+  //for directed graph
+  /// u----> v
+  ////time complexity --- O(E) here E is edge of a graph
+  for(int i = 0;i<m;i++){
+    int u,v;
+    cin >> u>> v;
+    adj[u].push_back(v);
+  }
+ 
+  // for weighted graph we will be store weight on the list of the matrix
+
   return 0;
 }
