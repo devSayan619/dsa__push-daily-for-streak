cf989f32650a7c21b0f4bfc99bc047372f224c68
devSayan619 <mainmandal23@gmail.com>
2024-09-11 19:57:35 +0530

    implepent of tree

---
+++ topic wise question/tree/impli_tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+  int data;
+  Node *left;
+  Node *right;
+  Node(int val)
+  {
+    data = val;
+    left = right = NULL;
+  }
+};
+
+int main()
+{
+  struct Node *root = new Node(11);
+  root->left = new Node(12);
+  root->right = new Node(15);
+  root->left->right = new Node(5);
+
+  return 0;
+}
