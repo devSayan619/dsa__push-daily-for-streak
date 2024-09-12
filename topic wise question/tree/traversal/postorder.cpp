d903ea912506d6de14cad1af669a0259ea1c59e6
devSayan619 <mainmandal23@gmail.com>
2024-09-12 15:30:44 +0530

    traversal

--- topic wise question/tree/traversal/postorder.cpp
+++ topic wise question/tree/traversal/postorder.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 struct Node
@@ -12,13 +13,40 @@ struct Node
     left = right = NULL;
   }
 };
+void postorder(Node *root, vector<int> &arr)
+{
+  if (root == nullptr)
+  {
+    return;
+  }
+  postorder(root->left, arr);
+  postorder(root->right, arr);
+  arr.push_back(root->data);
+}
+
+vector<int> postOrder(Node *root)
+{
+  vector<int> arr;
+
+  postorder(root, arr);
+  return arr;
+}
 
 int main()
 {
-  struct Node *root = new Node(11);
-  root->left = new Node(12);
-  root->right = new Node(15);
-  root->left->right = new Node(5);
+  Node *root = new Node(4);
+  root->left = new Node(2);
+  root->right = new Node(5);
+  root->left->left = new Node(6);
+  root->left->right = new Node(1);
+  root->right->right = new Node(7);
+
+  vector<int> result = postOrder(root);
+
+  for (int a : result)
+  {
+    cout << a << " ";
+  }
 
   return 0;
 }
--- topic wise question/tree/traversal/preorder.cpp
+++ topic wise question/tree/traversal/preorder.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 struct Node
@@ -12,13 +13,40 @@ struct Node
     left = right = NULL;
   }
 };
+void preorder(Node *root, vector<int> &arr)
+{
+  if (root == nullptr)
+  {
+    return;
+  }
+  arr.push_back(root->data);
+  preorder(root->left, arr);
+  preorder(root->right, arr);
+}
+
+vector<int> preOrder(Node *root)
+{
+  vector<int> arr;
+
+  preorder(root, arr);
+  return arr;
+}
 
 int main()
 {
-  struct Node *root = new Node(11);
-  root->left = new Node(12);
-  root->right = new Node(15);
-  root->left->right = new Node(5);
+  Node *root = new Node(4);
+  root->left = new Node(2);
+  root->right = new Node(5);
+  root->left->left = new Node(6);
+  root->left->right = new Node(1);
+  root->right->right = new Node(7);
+
+  vector<int> result = preOrder(root);
+
+  for (int a : result)
+  {
+    cout << a << " ";
+  }
 
   return 0;
 }
