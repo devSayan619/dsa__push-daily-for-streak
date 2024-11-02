926a77c84de694929dbdcb93b3ab14a676bd318f
devSayan619 <mainmandal23@gmail.com>
2024-11-02 19:04:14 +0000

    harshed number

---
+++ striver_nqt_sheet/numbers/num-pure/harshed_num.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter the number "<<endl;
+  cin>>n;
+  int temp=n;
+  int sum = 0;
+  while(temp>0){
+    sum+=temp%10; 
+    temp /= 10;
+  }
+  if(n%sum==0){
+    cout<<"Yes it is a Harshad number." <<endl;
+  }
+  else{
+    cout<<"it is not a Harshad number."<<endl;
+  }
+  
+  return 0;
+}
