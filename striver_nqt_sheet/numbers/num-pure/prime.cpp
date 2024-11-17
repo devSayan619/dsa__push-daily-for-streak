cb98342f0501d1f34a8edc978b8c059f3133a953
devSayan619 <mainmandal23@gmail.com>
2024-11-17 18:25:45 +0530

    fixing error

--- striver_nqt_sheet/numbers/num-pure/prime.cpp
+++ striver_nqt_sheet/numbers/num-pure/prime.cpp
@@ -33,7 +33,7 @@ bool isPrime(int n){
 bool isPrime(int n){
     if(n==1) return false;
 
-    if(n==2 || n==3 ) return false;
+    if(n==2 || n==3 ) return true;
 
     if(n%2 == 0 || n%3 == 0) return false;
 
